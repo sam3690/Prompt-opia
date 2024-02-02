12720292b6101a272c0453fbaed8fb64dd09b754
sam3690 <usamabinayoub@gmail.com>
2024-02-02 21:14:48 +0500

    First commit

---
+++ utils/database.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected = false; //track the connection
+
+export const connectToDB = () => {
+    mongoose.set('strictQuery', true);
+
+    if (isConnected) {
+        console.log('MongoDB is already connected');
+    }
+    try {
+        mongoose.connect(process.env.MONGODB_URI, {
+            dbName: 'share_prompt',
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        isConnected = true;
+
+        console.log('MongoDB connected');
+        
+    } catch (error) {
+        
+    }
+}
