388f46e73dc1934e54470e13355d57b672de5903
sam3690 <usamabinayoub@gmail.com>
2024-02-13 00:44:43 +0500

    updated 12:44 am 13-02-24

--- components/Form.jsx
+++ components/Form.jsx
@@ -5,6 +5,37 @@ const Form = ({  type,  post,  setPost,  submitting, handleSubmit }) => {
     <section className="w-full max-w-full flex-start flex-col">
       <h1 className="head_text text-left"><span className="blue_gradient">{type} Post</span></h1>
       <p className="desc text-left max-w-md">{type} and share amazing prompts with the world, and let your imagination run wild with any AI-powered platform</p>
+      <form 
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+      >
+        <label>
+          <span className="font-satoshi font-semibold text-bse text-gray-700">
+            Your AI Prompt
+          </span>
+          <textarea 
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post,
+            prompt: e.target.value})}
+            className="form_textarea"
+            placeholder="Enter your AI Prompt here..."
+          />
+        </label>
+        <label>
+          <span className="font-satoshi font-semibold text-bse text-gray-700">
+            Tag {' '}
+            <span className="font-normal">(#product, #webdevelopement, #idea)</span>
+          </span>
+          <textarea 
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post,
+            prompt: e.target.value})}
+            className="form_textarea"
+            placeholder="Enter your AI Prompt here..."
+          />
+        </label>
+
+      </form>
     </section>
   )
 }
